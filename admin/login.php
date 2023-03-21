3faaa7fbb1fa0d663c25cbf63436c339f246699b
Francisco Oliveira <75614393+itsfranciscooli@users.noreply.github.com>
2023-03-21 01:33:21 +0000

    Start V2

---
+++ admin/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Lnk Tree</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&amp;display=swap">
+    <link rel="stylesheet" href="../assets/css/styles.min.css?h=277cfa82206b88e2ed6a0995b25a443d">
+</head>
+
+<body style="background: var(--bs-gray-dark);">
+    <section class="py-4 py-xl-5">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <h2>Log in</h2>
+                </div>
+            </div>
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-5">
+                        <div class="card-body d-flex flex-column align-items-center">
+                            <div class="bs-icon-xl bs-icon-circle bs-icon-primary bs-icon my-4"></div>
+                            <div class="alert alert-danger" role="alert"><span>Wrong Password/User</span></div>
+                            <form class="text-center" method="post">
+                                <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                                <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+                                <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit">Login</button></div><a style="text-decoration: none;" href="../forgotpassword.php"><p class="text-muted">Forgot your password?</p></a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="text-muted mb-0" style="text-align: center;">Copyright © 2023 Lnk Tree</p>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Lnk Tree</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&amp;display=swap">
+    <link rel="stylesheet" href="assets/css/styles.min.css?h=277cfa82206b88e2ed6a0995b25a443d">
+</head>
+
+<body>
+    <div style="text-align: center;margin-top: 38px;margin-bottom: 38px;"><img src="assets/img/boy.jpg?h=a6f2754f1f2f95b6dda5170e292ecdf2" style="height: 80px;"></div>
+    <div style="text-align: center;margin-top: 10px;margin-bottom: 10px;"><button class="btn btn-primary" type="" style="text-align: center;width: 60%;height: 50px;font-family: 'Open Sans', sans-serif;font-weight: bold;border-radius: 0px;border-style: none;">bla bla link</button>
+        <div style="text-align: center;margin-top: 10px;margin-bottom: 10px;"><button class="btn btn-primary" type="" style="text-align: center;width: 60%;height: 50px;font-family: 'Open Sans', sans-serif;font-weight: bold;border-radius: 0px;border-style: none;">TEXT</button></div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
